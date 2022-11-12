0ad7ec74196933ee8cfd1c2bdcfd20232b83f325
Pavithrakuruba <103957063+Pavithrakuruba@users.noreply.github.com>
2022-11-12 16:34:08 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,34 @@
+# d1={'a':100 , 'b': 200 , 'c' : 300}
+# d2={'a':200,  'b':300,  'd':400}
+# d3={}
+# for i,j in d1.items():
+#     for x,y in d2.items():
+#         if i==x:
+#             d3[i]=(j+y)
+# print(d3)
+# d4={}
+# for k in (d1,d2):
+#     d4.update(k)
+# print(d4)
+
+
+
+
+
+
+# a="My name is kumar, and my friend's name is Dhamu"
+# i=0
+# count=0
+# b=a.split(" ")
+# print(b)
+# while i<len(b):
+#     if "is" in b[i]:
+#         count=count+1
+#     i=i+1
+# print(count)    
+
+
+
+# def ask_question(): 
+#     print(5*"\n""ek baar")
+# ask_question()
